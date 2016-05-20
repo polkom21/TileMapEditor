493ca686ecd27f4506b5baf3f555c17b674b6e87
polkom21 <polkom21@gmail.com>
2016-05-20 21:49:03 +0200

    Generate default libgdx project

---
+++ desktop/src/com/renieckik/tilemapeditor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.renieckik.tilemapeditor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.renieckik.tilemapeditor.TileMapEditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TileMapEditor(), config);
+	}
+}
