5ee3f86c3343412fa86a49e2941b4f9f863d5656
polkom21 <polkom21@gmail.com>
2016-05-22 22:41:45 +0200

    Define directory to workspace

--- core/src/com/renieckik/tilemapeditor/TileMapEditor.java
+++ core/src/com/renieckik/tilemapeditor/TileMapEditor.java
@@ -5,6 +5,8 @@ import com.renieckik.tilemapeditor.screens.ProjectsScreen;
 
 public class TileMapEditor extends Game {
 
+	public static final String programDir = System.getProperty("user.home") + "/AppData/Local/TileMapEditor/";
+	
 	@Override
 	public void create () {
 		this.setScreen(new ProjectsScreen(this));
