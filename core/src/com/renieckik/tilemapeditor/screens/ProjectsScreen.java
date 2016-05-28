09df62060883f5d18848bbab29c0367eeb0e5b22
polkom21 <polkom21@gmail.com>
2016-05-28 13:14:35 +0200

    Init VisUi and dispose it on close screen

--- core/src/com/renieckik/tilemapeditor/screens/ProjectsScreen.java
+++ core/src/com/renieckik/tilemapeditor/screens/ProjectsScreen.java
@@ -1,19 +1,25 @@
 package com.renieckik.tilemapeditor.screens;
 
+import com.kotcrab.vis.ui.VisUI;
 import com.renieckik.tilemapeditor.TileMapEditor;
 
 public class ProjectsScreen extends AbstractScreen {
 	
 	public ProjectsScreen(TileMapEditor editor) {
 		super(editor);
+		VisUI.load();
 	}
 	
 	@Override
 	public void render(float delta) {
 		super.render(delta);
-		
 		spriteBatch.begin();
 		spriteBatch.end();
+	}
+	
+	@Override
+	public void dispose() {
+		VisUI.dispose();
 	}
 
 }
