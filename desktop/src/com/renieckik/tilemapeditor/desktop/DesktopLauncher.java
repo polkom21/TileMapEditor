8fc2d04f5182103ddf94f931a0e35013b201f78b
polkom21 <polkom21@gmail.com>
2016-06-05 22:12:11 +0200

    Change default resolution on dekstop app to 720p (1280x720)

--- desktop/src/com/renieckik/tilemapeditor/desktop/DesktopLauncher.java
+++ desktop/src/com/renieckik/tilemapeditor/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.renieckik.tilemapeditor.TileMapEditor;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new TileMapEditor(), config);
 	}
 }
