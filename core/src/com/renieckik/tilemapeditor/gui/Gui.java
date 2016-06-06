08e00aba592db377db20c67866ea91b42a49607b
polkom21 <polkom21@gmail.com>
2016-06-06 22:53:41 +0200

    Init required VisUi and open ProjectViewScreen selected project

--- core/src/com/renieckik/tilemapeditor/gui/Gui.java
+++ core/src/com/renieckik/tilemapeditor/gui/Gui.java
@@ -3,6 +3,7 @@ package com.renieckik.tilemapeditor.gui;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.kotcrab.vis.ui.VisUI;
 
 public class Gui extends Stage {
 	
@@ -11,6 +12,8 @@ public class Gui extends Stage {
 	private MenuBarElement menu;
 	
 	public Gui() {
+		VisUI.load();
+		
 		table = new Table();
 		
 		menu = new MenuBarElement();
@@ -18,6 +21,11 @@ public class Gui extends Stage {
 		table.add(menu.getActor());
 		
 		this.addActor(table);
+	}
+	
+	@Override
+	public void dispose() {
+		VisUI.dispose();
 	}
 	
 }
