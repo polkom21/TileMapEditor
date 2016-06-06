1fcd5f85b1ab85daa6a759cceea4dbfd75cf9ac5
polkom21 <polkom21@gmail.com>
2016-06-06 23:18:58 +0200

    Add quit event to file menu.

--- core/src/com/renieckik/tilemapeditor/gui/MenuBarElement.java
+++ core/src/com/renieckik/tilemapeditor/gui/MenuBarElement.java
@@ -1,8 +1,12 @@
 package com.renieckik.tilemapeditor.gui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.kotcrab.vis.ui.widget.Menu;
 import com.kotcrab.vis.ui.widget.MenuBar;
+import com.kotcrab.vis.ui.widget.MenuItem;
 
 public class MenuBarElement extends GuiElement {
 	
@@ -14,6 +18,15 @@ public class MenuBarElement extends GuiElement {
 		menuBar = new MenuBar();
 		Menu file = new Menu("File");
 		menuBar.addMenu(file);
+		
+		MenuItem quit = new MenuItem("Quit");
+		quit.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.exit();
+			}
+		});
+		file.addItem(quit);
 	}
 	
 	public Table getActor() {
