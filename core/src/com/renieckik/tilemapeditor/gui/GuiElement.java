1dad874efc0eaca7c8d69eba4cf6f70137f22b1a
polkom21 <polkom21@gmail.com>
2016-06-06 22:22:33 +0200

    Create GuiElement with getter and setter to name.

---
+++ core/src/com/renieckik/tilemapeditor/gui/GuiElement.java
@@ -0,0 +1,13 @@
+package com.renieckik.tilemapeditor.gui;
+
+public abstract class GuiElement {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
