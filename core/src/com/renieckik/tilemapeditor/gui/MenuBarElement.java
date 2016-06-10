7d6b095207b1cd6fafeeac6bdf6b3840fab2273f
polkom21 <polkom21@gmail.com>
2016-06-10 13:41:32 +0200

    Open project by double click on projectsListView

--- core/src/com/renieckik/tilemapeditor/gui/MenuBarElement.java
+++ core/src/com/renieckik/tilemapeditor/gui/MenuBarElement.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.kotcrab.vis.ui.widget.Menu;
 import com.kotcrab.vis.ui.widget.MenuBar;
 import com.kotcrab.vis.ui.widget.MenuItem;
--- core/src/com/renieckik/tilemapeditor/screens/ProjectsListScreen.java
+++ core/src/com/renieckik/tilemapeditor/screens/ProjectsListScreen.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
@@ -46,6 +48,15 @@ public class ProjectsListScreen extends AbstractScreen {
 		
 		projectsList = new List<String>(skin);
 		projectsList.setItems(projectsNames);
+		
+		projectsList.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				if(getTapCount() > 1) {
+					editor.setScreen(new ProjectViewScreen(editor, projects.get(projectsList.getSelectedIndex())));
+				}
+			}
+		});
 		
 		container.add(projectsList).colspan(3).grow();
 		container.row();
